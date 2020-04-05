4c48ac13f5b1f1937c80e639155784ed91dbbd51
Qi Ji <qjqqyy@gmail.com>
2020-04-06 00:59:28 +0800

    fix the damn CI

--- scripts/diff_psql.sh
+++ scripts/diff_psql.sh
@@ -10,11 +10,13 @@ docker exec cs3223 psql -U postgres -f '/cs3223/postgres/schema.sql'
 docker exec cs3223 psql -U postgres -f '/cs3223/postgres/values.sql'
 
 cd work
+FAILED=0
 for i in query*.txt; do
   echo "===> CHECKING $i <==="
   sed -e "/WHERE/s/,/ AND /g;s/\"/'/g" ${i//.txt/.sql} | \
     docker exec -i cs3223 psql -U postgres -f - --csv -A -F "	" -t -o "/cs3223/work/expected_$i"
-  diff -u <(sort expected_$i) <(sed -e '1d;s/\t$//' $i | sort)
+  diff -u <(sort expected_$i) <(sed -e '1d;s/\t$//' $i | sort) || FAILED=1
 done
 
 docker stop cs3223
+exit $FAILED
